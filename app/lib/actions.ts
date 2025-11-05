343dc31f2985b2c3846220e06792fe244f453751
sapopo888 <sapopo0813@yahoo.co.jp>
2025-11-05 14:45:37 +0900

    データベースが更新されると/dashboard/invoicesパスが再検証されて最新のデータ取得

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,7 @@
 "use server"; //ファイル内のエクスポートされたすべての関数をサーバーアクションとしてマークする
 
 import { z } from "zod";
+import { revalidatePath } from "next/cache"; 
 import postgres from 'postgres';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require'});
@@ -29,4 +30,6 @@ export async function createInvoice(formData: FormData) {
   await sql`
   INSERT INTO invoices (customer_id, amount, status, date)
   VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
+
+  revalidatePath('/dashboard/invoices');
 }
