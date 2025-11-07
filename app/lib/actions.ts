a4648825b6d39f899c2cf6265853455ed546a333
sapopo888 <sapopo0813@yahoo.co.jp>
2025-11-07 13:17:35 +0900

    請求書更新する関数記載

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -18,6 +18,8 @@ const FormSchema = z.object({
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true }); // ③omit(xxx)で記述したxxxを除外して新しいスキーマを作成(使うものだけ取得できる)
 
+const UpdateInvoice = FormSchema.omit({ id: true, date: true }); // invoice更新する用
+
 // ④CreateInvoiceを使って型検証
 export async function createInvoice(formData: FormData) {
   const {customerId, amount, status} = CreateInvoice.parse({
@@ -34,4 +36,23 @@ export async function createInvoice(formData: FormData) {
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices'); // リダイレクト
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
