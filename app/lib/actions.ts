b58230007654cfb94ca7486aa7e7e5c458577e20
omori saho <omorisaho@omorinoMacBook-Pro.local>
2025-11-08 22:38:14 +0900

    請求書更新に例外処理追加

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -41,7 +41,7 @@ export async function createInvoice(formData: FormData) {
       message: 'Database Error: Failed to Create Invoice.',
     };
   }
-  
+
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices'); // リダイレクト
 }
@@ -54,13 +54,18 @@ export async function updateInvoice(id: string, formData: FormData) {
   });
  
   const amountInCents = amount * 100;
- 
-  await sql`
+
+  try {
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
- 
+  } catch (error) {
+    console.error(error);
+    return { message: 'Database Error: Failed to Update Invoice.' };
+  }
+
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
