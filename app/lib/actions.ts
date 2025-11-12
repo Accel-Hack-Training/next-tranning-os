39dc28b4dde7848340310ecbc55d6bd14882b3c1
sapopo888 <sapopo0813@yahoo.co.jp>
2025-11-12 11:11:32 +0900

    zodでフォームデータの検証

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -10,9 +10,14 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require'});
 // ②スキーマの定義
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(), // z.coerceで数値型に強制変換
-  status: z.enum(["pending", "paid"]),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer',
+  }),
+  amount: z.coerce.number()// z.coerceで数値型に強制変換
+                  .gt(0, { message: 'Please enter an amount greater than $0.'}), //常に0より大きくなるようにする(gt: greater thanの略, >)
+  status: z.enum(["pending", "paid"], {
+    invalid_type_error: 'Please select an invoice status',
+  }),
   date: z.string(),
 });
 
